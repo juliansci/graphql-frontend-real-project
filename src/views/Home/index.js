7aad22548d9e8385baa1fa32b8f8f5d57f90c163
Julián Scialabba <julian.scialabba@gmail.com>
2020-09-19 21:01:17 -0300

    Apollo GraphQL Integration

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,77 +1,34 @@
 import React from 'react';
+import { useQuery, gql } from '@apollo/client';
 import PageTitle from '../../components/PageTitle';
 import TeamCard from '../../components/TeamCard';
 import { TeamsContainer } from './styled';
 
-const teams = [
-  {
-    name: 'Boca',
-    logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7f/Escudo_del_Club_Atl%C3%A9tico_Boca_Juniors_2012.svg/275px-Escudo_del_Club_Atl%C3%A9tico_Boca_Juniors_2012.svg.png'
-  },
-  {
-    name: 'River',
-    logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fc/Escudo_del_Club_Atl%C3%A9tico_River_Plate.png/256px-Escudo_del_Club_Atl%C3%A9tico_River_Plate.png'
-  },
-  {
-    name: 'Boca',
-    logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7f/Escudo_del_Club_Atl%C3%A9tico_Boca_Juniors_2012.svg/275px-Escudo_del_Club_Atl%C3%A9tico_Boca_Juniors_2012.svg.png'
-  },
-  {
-    name: 'River',
-    logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fc/Escudo_del_Club_Atl%C3%A9tico_River_Plate.png/256px-Escudo_del_Club_Atl%C3%A9tico_River_Plate.png'
-  },
-  {
-    name: 'Boca',
-    logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7f/Escudo_del_Club_Atl%C3%A9tico_Boca_Juniors_2012.svg/275px-Escudo_del_Club_Atl%C3%A9tico_Boca_Juniors_2012.svg.png'
-  },
-  {
-    name: 'River',
-    logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fc/Escudo_del_Club_Atl%C3%A9tico_River_Plate.png/256px-Escudo_del_Club_Atl%C3%A9tico_River_Plate.png'
-  },
-  {
-    name: 'Boca',
-    logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7f/Escudo_del_Club_Atl%C3%A9tico_Boca_Juniors_2012.svg/275px-Escudo_del_Club_Atl%C3%A9tico_Boca_Juniors_2012.svg.png'
-  },
-  {
-    name: 'River',
-    logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fc/Escudo_del_Club_Atl%C3%A9tico_River_Plate.png/256px-Escudo_del_Club_Atl%C3%A9tico_River_Plate.png'
-  },
-  {
-    name: 'Boca',
-    logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7f/Escudo_del_Club_Atl%C3%A9tico_Boca_Juniors_2012.svg/275px-Escudo_del_Club_Atl%C3%A9tico_Boca_Juniors_2012.svg.png'
-  },
-  {
-    name: 'River',
-    logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fc/Escudo_del_Club_Atl%C3%A9tico_River_Plate.png/256px-Escudo_del_Club_Atl%C3%A9tico_River_Plate.png'
-  },
-  {
-    name: 'Boca',
-    logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7f/Escudo_del_Club_Atl%C3%A9tico_Boca_Juniors_2012.svg/275px-Escudo_del_Club_Atl%C3%A9tico_Boca_Juniors_2012.svg.png'
-  },
-  {
-    name: 'River',
-    logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fc/Escudo_del_Club_Atl%C3%A9tico_River_Plate.png/256px-Escudo_del_Club_Atl%C3%A9tico_River_Plate.png'
-  },
-  {
-    name: 'Boca',
-    logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7f/Escudo_del_Club_Atl%C3%A9tico_Boca_Juniors_2012.svg/275px-Escudo_del_Club_Atl%C3%A9tico_Boca_Juniors_2012.svg.png'
-  },
-  {
-    name: 'River',
-    logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fc/Escudo_del_Club_Atl%C3%A9tico_River_Plate.png/256px-Escudo_del_Club_Atl%C3%A9tico_River_Plate.png'
+const getTeamsQuery = gql`
+  query GetTeams {
+    getTeams{
+      id
+      name
+      logo
+    }
   }
-];
-const Home = () => (
-  <>
+`;
+
+const Home = () => {
+  const { loading, error, data } = useQuery(getTeamsQuery);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+  const teams = data.getTeams;
+  return <>
     <PageTitle>Teams</PageTitle>
     <TeamsContainer>
-      {teams.map(team => (
-        <TeamCard team={team} />
+      {teams.map((team, i) => (
+        <TeamCard team={team} key={i} />
       ))
       }
     </TeamsContainer>
   </>
-);
+};
 
 
 export default Home;
